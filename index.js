202fb496b3025827860d037b8a035c0be4808750
dheim123 <dheim@midland.k12.ia.us>
2025-10-23 11:59:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+var distanceInMiles = 100;
+
+function estimatedArrivalTime (){
+  let milesPerHour = 50;
+  return distanceInMiles / milesPerHour;
+}
+
+document.getElementById("result").innerHTML = estimatedArrivalTime;
 
